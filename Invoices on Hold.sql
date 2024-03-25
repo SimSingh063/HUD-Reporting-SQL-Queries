a7f9debd74bb4e7b267aa20226390750a1c9dd2e
Simranjeet Singh <simranjeet.singh@hud.govt.nz>
2024-03-26 09:53:11 +1300

    added name type = global in the report

--- Invoices on Hold.sql
+++ Invoices on Hold.sql
@@ -30,13 +30,12 @@ FROM (
         ap_invoices_all aia    
         INNER JOIN ap_holds_all aha ON aha.invoice_id = aia.invoice_id    
         INNER JOIN ap_lookup_codes alc ON alc.lookup_code = aha.hold_lookup_code 
-        INNER JOIN po4z_suppliers pz ON aia.vendor_id = pz.vendor_id
+        INNER JOIN poz_suppliers pz ON aia.vendor_id = pz.vendor_id
         INNER JOIN hz_parties hp ON pz.party_id = hp.party_id 
         INNER JOIN hr_organization_units_f_tl hou ON hou.organization_id = aia.org_id
     WHERE    
         aha.release_lookup_code IS NULL    
         AND alc.lookup_type = 'HOLD CODE'  
-        AND aha.hold_lookup_code <> 'INCOMPLETE INVOICE'
     ) inv_hold   
     LEFT JOIN po_line_locations pll ON pll.line_location_id = inv_hold.line_location_id  
     LEFT JOIN po_lines_all pla ON pla.po_header_id = pll.po_header_id AND pla.po_line_id = pll.po_line_id 
@@ -45,6 +44,7 @@ FROM (
     LEFT JOIN per_person_names_f ppn ON pod.deliver_to_person_id = ppn.person_id
 WHERE 
     inv_hold.invoice_amount <> 0
+    AND ppn.name_type = 'GLOBAL'
     AND (COALESCE(NULL, :HoldReason) IS NULL OR inv_hold.displayed_field IN (:HoldReason))
     AND (COALESCE(NULL, :BusinessUnit) IS NULL OR inv_hold.Business_Unit IN (:BusinessUnit))
 
@@ -61,4 +61,3 @@ WHERE
     AND alc.lookup_type = 'HOLD CODE'  
     AND aha.hold_lookup_code <> 'INCOMPLETE INVOICE'
     
-
